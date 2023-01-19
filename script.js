1d4d8faa9079eaaa2bb05e73079cacc9fe9aed1d
Kerrish <105024654+Kerrish@users.noreply.github.com>
2023-01-19 02:34:50 +0000

    Arrays

--- script.js
+++ script.js
@@ -21,7 +21,39 @@ let linkDisplay = document.querySelector(".display-link");
 console.log(linkDisplay)
 // task 7: create and populate an array to store your image urls. Create three more arrays. One to store your song names, one for the artists, and a last one for the song links.
 
+let songNameList = [
+  "How Far I’ll Go",
+  "Let it Go",
+  "Reflection",
+  "We don’t talk about Bruno",
+  "Colors of the Wind"
+]
 
+let artists = [
+  "Ella Mai",
+  "Auli'i Cravalho",
+  "Idina Menzel",
+  "Christina Aguilera",
+  "Encanto - Cast, Rhenzy Feliz, Adassa, Mauro Castillo, Stephanie Beatriz, Diane Guerrero, Carolina Gaitán",
+  "Judy Kuhn",
+]
+
+let songLinkList = [
+  "https://www.youtube.com/watch?v=6YNZlXfW6Ho",
+  "https://www.youtube.com/watch?v=cPAbx5kgCJo",
+  "https://www.youtube.com/watch?v=L0MK7qz13bU",
+  "https://www.youtube.com/watch?v=lGGXsm0a5s0",
+  "https://www.youtube.com/watch?v=bvWRMAU6V-c",
+  "https://www.youtube.com/watch?v=O9MvdMqKvpU"
+]
+
+let imgURL = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8-hLZvl7VWw60VWNs3_801dskbtKA4-x8Bw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuJp5f5Fd4MoNoPwe_6VpB9sse2Sb0qHdK-A&usqp=CAU",
+  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fblog.tutorabcchinese.com%2Fhs-fs%2Fhubfs%2Ftrue-facts-about-the-real-mulan.jpg%3Fwidth%3D1023%26name%3Dtrue-facts-about-the-real-mulan.jpg&imgrefurl=https%3A%2F%2Fblog.tutorabcchinese.com%2Fexpats%2Ffacts-about-the-real-mulan&tbnid=VlOnFSi7CCbQ2M&vet=12ahUKEwjJq7eWx9L8AhXJPEQIHXEUD1IQMygSegUIARCOAg..i&docid=At-1nQG9ccTGOM&w=1023&h=628&q=mulan&ved=2ahUKEwjJq7eWx9L8AhXJPEQIHXEUD1IQMygSegUIARCOAg",
+  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.bauerhosting.com%2Flegacy%2Fmedia%2F61f1%2F1dc7%2F2ea3%2F9557%2F7c07%2F8954%2Fbruno-1.jpg%3Fformat%3Djpg%26quality%3D80%26width%3D960%26height%3D540%26ratio%3D16-9%26resize%3Daspectfill&imgrefurl=https%3A%2F%2Fwww.empireonline.com%2Fmovies%2Ffeatures%2Fmaking-of-we-dont-talk-about-bruno-encanto-biggest-hit%2F&tbnid=VgBeNPV8u5xH1M&vet=12ahUKEwjS8c_Nx9L8AhWQOEQIHQGKDlcQMygHegUIARD1AQ..i&docid=wr7QW9rmgIZJhM&w=960&h=540&q=encanto%20we%20don%27t%20talk%20about%20bruno&ved=2ahUKEwjS8c_Nx9L8AhWQOEQIHQGKDlcQMygHegUIARD1AQ",
+  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FO9MvdMqKvpU%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DO9MvdMqKvpU&tbnid=hgQ5DvhwDPJKiM&vet=12ahUKEwiL9KXhx9L8AhXyK0QIHYcYA_oQMygAegUIARDpAQ..i&docid=GPRWRPurZ2_TdM&w=1280&h=720&q=colors%20of%20the%20wind&ved=2ahUKEwiL9KXhx9L8AhXyK0QIHYcYA_oQMygAegUIARDpAQ"
+];
 
 
 
